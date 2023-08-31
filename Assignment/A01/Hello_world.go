49cd0e902d8aff49156870c456ae05887fd00536
Ahmed <ahmedkrubally696@gmail.com>
2023-08-31 12:07:55 -0500

    commit

--- Assignment/A01/Hello_world.go
+++ Assignment/A01/Hello_world.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
-func main( ){
+func main() {
 	fmt.Println("Hello world! this is my first GO program ever.")
+
+	var halfevil int = 333
+
+	fmt.Print("The number for half evil is : ", halfevil, " The best fucking number in world or whateva.")
+	fmt.Printf("\n")
+	//looping thorugh and array
+	number := []int{2, 4, 6, 8, 10}
+	for _, num := range number {
+		fmt.Print(num, " ")
+	}
 }
