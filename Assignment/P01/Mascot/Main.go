2668d9b1b97967e79189806e1af75fc342120c36
Med0sin <101606784+Med0sin@users.noreply.github.com>
2023-12-17 17:00:30 -0600

    Update Main.go

--- Assignment/P01/Mascot/Main.go
+++ Assignment/P01/Mascot/Main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	// "github.com/Med0sin/programming_lang_go/tree/main/Assignment/P01"
+	"github.com/Med0sin/programming_lang_go/tree/main/Assignment/P01"
 	"rsc.io/quote"
 )
 
